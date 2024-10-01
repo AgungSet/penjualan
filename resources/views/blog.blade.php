f47d3a808ed2f4a579f08fc6e2aaedf19fef981b
AgungSet <116327716+AgungSet@users.noreply.github.com>
2024-10-01 17:16:06 +0700

    2

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,13 @@
+<article>
+<h3>judul 1</h3>
+<p>ygkhjgjhgjhkhfhgfgdgfdgstryjfhftyrtyrgfgdtrdyf</p>
+</article>
+
+<article>
+    <a href="/">Home</a>
+    <a href="/blog">blog</a>
+    <a href="/about">About</a>
+    <a href="/kontak">kontak</a>
+    <h2>judul 2</h2>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis laborum, tempore nam consectetur esse deserunt, consequuntur rerum fuga doloremque optio numquam at obcaecati iure quod, voluptas amet perspiciatis voluptatum! Dolorem.
+</article>
---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">blog</a>
+    <a href="/about">About</a>
+    <a href="/kontak">kontak</a>
+    <h1>halam kontak</h1>
+    <ul>
+        <li>
+            <a href="https://instagram.com">instagram</a>
+        </li>
+
+    </ul>
+</body>
+</html>
