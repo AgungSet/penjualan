d7923db772f1928e98ca5a45c7385e21ee6f4f80
AgungSet <116327716+AgungSet@users.noreply.github.com>
2024-10-02 15:56:13 +0700

    y

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,16 @@ export default defineConfig({
         }),
     ],
     build: {
-        outDir: 'public/build',
+        manifest: true, // Ini akan menghasilkan manifest.json
+        outDir: 'public/build', // Hasil build akan disimpan di folder ini
+        rollupOptions: {
+            input: { // Jika Anda ingin menentukan beberapa file input, lakukan di sini
+                app: 'resources/js/app.js',
+                css: 'resources/css/app.css',
+            },
+            output: {
+                // Anda bisa menambahkan opsi output jika perlu
+            }
+        }
     },
 });
